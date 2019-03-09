162a64485463053fd4f5c474ba1a87ca3083cee3
rodrigost98 <rodrisu989@gmail.com>
2019-03-09 13:50:59 -0400

    con modelo de producto

---
+++ producto2/producto2/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace producto2.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<producto2.Models.producto> productoes { get; set; }
+    }
+}
---
+++ producto2/producto2/Models/producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace producto2.Models
+{
+    public class producto
+    {
+        public int productoID { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+    }
+}
